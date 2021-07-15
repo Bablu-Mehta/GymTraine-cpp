bf15d69a22b7d404f8f85bfc1116e1ea20fa59cc
Bablu-Mehta <babloomehta895012@gmail.com>
2021-07-15 15:10:45 +0530

    Changes

---
+++ GymTraine.cpp
@@ -0,0 +1,374 @@
+#include<conio.h>
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+class man
+{
+	public:
+void monday()
+{
+	cout<<"\t\tChest day"<<endl;
+	cout<<"\t\tFirst of all Stretch your body"<<endl;
+	cout<<"\t\t1. Barbell Bench Press"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	cout<<"\t\t2. Inclined Dumbbell Press"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t   Rest a little bit after the sets"<<endl;
+	cout<<"\t\t3. Dips"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t   Rest a little bit after the sets"<<endl;
+	cout<<"\t\t4. Pullups"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+}
+void tuesday()
+{       cout<<"\t\tShoulders and Arms day"<<endl;
+	cout<<"\t\tFirst of all Stretch your body"<<endl;
+	cout<<"\t\t1. Military Press or Dumbbell Press"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	cout<<"\t\t2. Lateral Raises"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	cout<<"\t\t3. Barbell Curls"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	cout<<"\t\t4. Dumbell Curls"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+
+}
+void wednesday()
+{       cout<<"\t\tBiceps day"<<endl;
+	cout<<"\t\tFirst of all Stretch your body"<<endl;
+	cout<<"\t\t1. Incline Dumbell Hammer curl"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	cout<<"\t\t2. Incline Inner-Biceps curl "<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	cout<<"\t\t3. Zottman Curl"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	cout<<"\t\t4. EZ-Bar curl"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+}
+void thursday()
+{
+	cout<<"\t\tTriceps day"<<endl;
+	cout<<"\t\tFirst of all Stretch your body"<<endl;
+	cout<<"\t\t1. Close-Grip Barbell Bench Press"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	cout<<"\t\t2. Dip Machine"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	cout<<"\t\t3. Seated Dumbell Press"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	cout<<"\t\t4. V-Bar Pulldown"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+}
+void friday()
+{
+	cout<<"\t\tBack day"<<endl;
+	cout<<"\t\tFirst of all Stretch your body"<<endl;
+	cout<<"\t\t1. Band Bent-over Row"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	cout<<"\t\t2. Renegade Row"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	cout<<"\t\t3. Dumbbell Single Arm Row"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	cout<<"\t\t4. Inverted Row"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+}
+void saturday()
+{
+	cout<<"\t\tLeg Day"<<endl;
+	cout<<"\t\tFirst of all Stretch your body"<<endl;
+	cout<<"\t\t1. Barbell Squat"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	cout<<"\t\t2. Dumbell Lunges"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	cout<<"\t\t3. Leg Press"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	cout<<"\t\t4. Standing Calf Raises"<<endl;
+	cout<<"\t\t\t * 3-4 sets of 6-8 reps or 10-12 reps"<<endl;
+	cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+}
+};
+class woman
+{       public:
+	void wmonday()
+	{
+		cout<<"\t\tChest day"<<endl;
+		cout<<"\t\tFirst of all Stretch your body"<<endl;
+		cout<<"\t\t1. Standing Chest Stretch"<<endl;
+		cout<<"\t\t\t * Hold the position for 2o-3o seconds"<<endl;
+		cout<<"\t\t\t * 2 sets of 5 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t2. Camel Pose"<<endl;
+		cout<<"\t\t\t * Hold the position for 3o seconds"<<endl;
+		cout<<"\t\t\t * 2 sets of 5 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t3. Seated Dumbell Fly"<<endl;
+		cout<<"\t\t\t * 2 sets of 12 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t4. Lying Chest Fly"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+	}
+       void wtuesday()
+       {
+		cout<<"\t\tBiceps day"<<endl;
+		cout<<"\t\tFirst of all Stretch your body"<<endl;
+		cout<<"\t\t1. Biceps Curl"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t2. Hammer Curl"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t3. Curtsy Lunge With Biceps Curl"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t4. Squat With Biceps Curl"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+       }
+       void wwednesday()
+       {
+		cout<<"\t\tTriceps Day"<<endl;
+		cout<<"\t\tFirst of All Stretch your body a little bit"<<endl;
+		cout<<"\t\t1. Triceps Extension"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t2. Skull Crusher or Lying Triceps Extension"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t3. Triceps Dips"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t4. Triceps Push-ups"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+       }
+       void wthursday()
+       {
+		cout<<"\t\tBack and Wings day"<<endl;
+		cout<<"\t\tFirst of all Stretch your body"<<endl;
+		cout<<"\t\t1. Spreading Wings Lift"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t2. one Arm Rowing Dumbbell"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t3. Bow And Arrow"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t4. Back Arm Walk"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+       }
+       void wfriday()
+       {
+		cout<<"\t\tShoulders Day"<<endl;
+		cout<<"\t\tFirst of All stretch your body a little bit"<<endl;
+		cout<<"\t\t1. Dumbbell Front Raises"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t2. Standing Shoulder Press"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t3. Lateral Raises"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t4. Bent Arm Lateral Raises"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+       }
+       void wsaturday()
+       {
+		cout<<"\t\tLeg day"<<endl;
+		cout<<"\t\tFirst of all Stretch your body"<<endl;
+		cout<<"\t\t1. Goblet Squat"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t2. Banded Lateral Walk"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t3. Single Leg Deadlift"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+		cout<<"\t\t4. Lateral Lunge"<<endl;
+		cout<<"\t\t\t * 3 sets of 10 reps"<<endl;
+		cout<<"\t\t\t  Rest a little bit after the sets"<<endl;
+       }
+};
+class details:public man, public woman
+{
+	public:
+	void detail1()
+	{
+
+		int ch;
+		do
+		{
+		cout<<"\t\t|=====================================|"<<endl;
+		cout<<"\t\t|   Welcome to my gym Exercises       |"<<endl;
+		cout<<"\t\t|   Exercises for Male                |"<<endl;
+		cout<<"\t\t|   Press [0] for exit the page       |"<<endl;
+		cout<<"\t\t|   Press [1] for the day 1 routine   |"<<endl;
+		cout<<"\t\t|   Press [2] for the day 2 routine   |"<<endl;
+		cout<<"\t\t|   Press [3] for the day 3 routine   |"<<endl;
+		cout<<"\t\t|   Press [4] for the day 4 routine   |"<<endl;
+		cout<<"\t\t|   Press [5] for the day 5 routine   |"<<endl;
+		cout<<"\t\t|   Press [6] for the day 6 routine   |"<<endl;
+		cout<<"\t\t|=====================================|"<<endl;
+		cin>>ch;
+		switch(ch)
+		{
+			case 1:
+			{
+				monday();
+				break;
+			}
+			case 2:
+			{
+				tuesday();
+				break;
+			}
+			case 3:
+			{
+				wednesday();
+				break;
+			}
+			case 4:
+			{
+				thursday();
+				break;
+			}
+			case 5:
+			{
+				friday();
+				break;
+			}
+			case 6:
+			{
+				saturday();
+				break;
+			}
+			default:
+			{
+				cout<<"Thank you for your valuable time"<<endl;
+			}
+		}
+		 }
+		 while(ch!=0);
+
+	}
+
+	void detail2()
+	{
+		int ch;
+		do
+		{
+		cout<<"\t\t|=====================================|"<<endl;
+		cout<<"\t\t|   Welcome to my gym Exercises       |"<<endl;
+		cout<<"\t\t|   Exercises for Female              |"<<endl;
+		cout<<"\t\t|   Press [0] for exit the page       |"<<endl;
+		cout<<"\t\t|   Press [1] for the day 1 routine   |"<<endl;
+		cout<<"\t\t|   Press [2] for the day 2 routine   |"<<endl;
+		cout<<"\t\t|   Press [3] for the day 3 routine   |"<<endl;
+		cout<<"\t\t|   Press [4] for the day 4 routine   |"<<endl;
+		cout<<"\t\t|   Press [5] for the day 5 routine   |"<<endl;
+		cout<<"\t\t|   Press [6] for the day 6 routine   |"<<endl;
+		cout<<"\t\t|=====================================|"<<endl;
+		cin>>ch;
+		switch(ch)
+		{
+			case 1:
+			{
+			       wmonday();
+				break;
+			}
+			case 2:
+			{
+				wtuesday();
+				break;
+			}
+			case 3:
+			{
+				wwednesday();
+				break;
+			}
+			case 4:
+			{
+				wthursday();
+				break;
+			}
+			case 5:
+			{
+				wfriday();
+				break;
+			}
+			case 6:
+			{
+				wsaturday();
+				break;
+			}
+			default:
+			{
+				cout<<"Thank you for your valuable time"<<endl;
+			}
+		}
+		 }
+		 while(ch!=0);
+
+	}
+
+
+};
+int main()
+{
+ char name[50], age[10], gender;
+ details d;
+ cout<<"\t\t\t----------------------------"<<endl;
+ cout<<"\t\t\t   Enter Your Name:-";
+ cin.getline(name,100);
+ cout<<endl;
+ cout<<"\t\t\t   Enter Your Age:-";
+ cin.getline(age,10);
+ cout<<endl;
+ cout<<"\t\t\t   Enter Your Gender(m/f):-";
+ cin>>gender;
+ cout<<"\t\t\t----------------------------"<<endl;
+switch(gender)
+{
+	case 'm':
+	{
+	d.detail1();
+	break;
+	}
+	case 'f':
+	{
+	d.detail2();
+	break;
+	 }
+	 default:
+	 {
+		cout<<"This is unspecified Gender";
+	 }
+	 }
+        return 0;
+}
